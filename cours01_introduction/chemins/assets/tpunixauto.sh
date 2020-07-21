d6225f3903de5c34a68978a120c0972e2393df11
cprevost <celine.prevost@univ-orleans.fr>
2020-07-21 14:54:39 +0200

    fix

--- cours01_introduction/chemins/assets/tpunixauto.sh
+++ cours01_introduction/chemins/assets/tpunixauto.sh
@@ -3,7 +3,6 @@ env > /tmp/.env_$(whoami)
 history -a
 }
 alias cd='echo La commande cd est cassé pour ce scénario. Je ne peux pas te déplacer vers '
-alias passage_secret='export PWD="/home/sasha/Desktop"'
 export PATH=$PATH:~/bin/
 shopt -s histappend
 PROMPT_COMMAND=prompt
